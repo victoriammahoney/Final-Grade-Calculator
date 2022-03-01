a2fc199d7bddd7584fe43bdac6588f6061e0424c
Victoria Mahoney <63237@psdschools.org>
2022-03-01 14:18:40 -0700

    mvp

--- Final-Grade-Calculator/ViewController.swift
+++ Final-Grade-Calculator/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var currentGradeLabel: UILabel!
+    
+    @IBOutlet weak var desiredGradeLabel: UILabel!
+    
+    @IBOutlet weak var finalExamWeightLabel: UILabel!
+    
+    @IBOutlet weak var currentTextField: UITextField!
+    
+    @IBOutlet weak var desiredTextField: UITextField!
+    
+    @IBOutlet weak var finalExamTextField: UITextField!
+    
+    @IBOutlet weak var calculateButtonOutlet: UIButton!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultLabel.isHidden = true
     }
 
+   
+    @IBAction func calculateButtonAction(_ sender: Any) {
+        
+        var currentGrade = currentTextField.text ?? ""
+        var c = Int(currentGrade) ?? 90
+        
+        var desiredGrade = desiredTextField.text ?? ""
+        var d = Int(desiredGrade) ?? 90
+        
+        var finalExam = finalExamTextField.text ?? ""
+        var f = Int(finalExam) ?? 90
+        
+        var finalGrade = d - (100 - f) * c / f
+        
+        resultLabel.text = "You need to score " + "\(finalGrade)" + "% on the Final Exam"
+        
+        resultLabel.isHidden = false
+    }
+    
+    
 
 }
 
